cbce8d56732e6c438e43ed6af05d5266bb72d543
ravi <mail.meravi00@gmail.com>
2018-08-08 21:55:29 +0545

    ViewModel for List implemented

--- TestApp/Controllers/EmployeeController.cs
+++ TestApp/Controllers/EmployeeController.cs
@@ -11,25 +11,31 @@ namespace TestApp.Controllers
     {
         public ActionResult getEmpView()
         {
-            Employee emp = new Employee();
-            emp.FirstName = "Ravi";
-            emp.LastName = "Maharjan";
-            emp.Salary = 20000;
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
 
-            EmployeeViewModel vmEmp = new EmployeeViewModel();
-            vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
-            vmEmp.Salary = emp.Salary.ToString("C");
-            if (emp.Salary > 15000)
-            {
-                vmEmp.SalaryColor = "yellow";
-            }
-            else
-            {
-                vmEmp.SalaryColor = "green";
-            }
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
 
-            vmEmp.UserName = "Admin";
-            return View("EmployeeView", vmEmp);
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            employeeListViewModel.UserName = "Admin";
+            return View("EmployeeView", employeeListViewModel);
             
         }
     }
--- TestApp/ViewModels/EmployeeViewModel.cs
+++ TestApp/ViewModels/EmployeeViewModel.cs
@@ -10,7 +10,7 @@ namespace TestApp.ViewModels
         public string EmployeeName { get; set; }
         public string Salary { get; set; }
         public string SalaryColor { get; set; }
-        public string UserName { get; set; }
+        
 
     }
 }
