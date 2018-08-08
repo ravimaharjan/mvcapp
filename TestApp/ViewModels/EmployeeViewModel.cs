cd81994b616c1f5f7be02991400786a43775b462
ravi <mail.meravi00@gmail.com>
2018-08-08 21:32:36 +0545

    ViewModels added

---
+++ TestApp/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public string EmployeeName { get; set; }
+        public string Salary { get; set; }
+        public string SalaryColor { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
